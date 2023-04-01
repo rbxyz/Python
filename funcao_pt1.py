a10eeabda694e7439eb8aca83ea474e7b5b24cf7
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-03-31 22:15:14 -0300

    Create funcao_pt1.py

---
+++ funcao_pt1.py
@@ -0,0 +1,17 @@
+#funcao é um dispositivo útil que agrupa um conjunto de instruções 
+# para que elas possam ser executadas mais de uma vez
+#    def nomeFuncao([parâmetro formal 1, parâmetro formal 2, ...]):
+#    bloco de instruções
+#    [return] valor
+
+def hello(meu_nome):
+  print('Olá',meu_nome)
+  hello('Fabio')
+[return] hello('Fabio')
+helllo("Olá Fabio")
+#return meu_nome
+#'Fabio'
+  
+def hello(meu_nome,idade):
+   print('Olá',meu_nome,'\nSua idade é:',idade)
+
