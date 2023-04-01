9b50f93e6b7e0ccc462ed0fa3174bd30507c064b
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-03-31 22:10:12 -0300

    Create while.py

---
+++ while.py
@@ -0,0 +1,22 @@
+x = 0
+while x < 10:
+    print('x is currently: ', x)
+    print('x is still less than 10, adding 1 to x')
+    x += 1
+    if x == 3:
+        print('breaking because x == 3')
+        break
+    else:
+        print('continuing...')
+        continue
+    #usando break
+    #break : para o loop
+    #continue : vai pro proximo loop
+    #pass : nao faz nada
+
+    
+
+
+
+
+
