8949b3e6e8396f665a24ee7ce4db38589f495c9b
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-03-31 22:21:47 -0300

    Create string_pt1.py

---
+++ string_pt1.py
@@ -0,0 +1,41 @@
+'hello'
+'isso tbm é uma frase'
+"string com aspas duplas"
+print('hello world')
+print('hello world 2')
+print('Use \n para inserir uma nova linha')
+print('nova linha')
+#usar a função len() para verificar o tamanho de uma string
+len('Hello World')
+s = 'hello world'
+s
+print(s)
+#vai mostrar s declarada como string
+
+#mostra o 1º elemento(nesse caso uma letra)
+s[0]
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+#':' serve para mostrar tudoo a partir de tal letra se for posto após(s[0]:), também dá pra fazer (s[:0])
+print(s[4:])
+print(s[:5])
+#muito ocorre:
+print(s[:])
+#também pode usar indexação negativa
+print(s[-1])
+#pega tudo, menos a ultima letra:
+print(s[:-1])
+
+#pode usar também para pegar de tantas em tantas letrs, tipo ::1
+print(s[::1])
+print(s[::2])
+print(s[::-1])
+print('exercicios: ')
+print(s[::-1])
+print('exrecicio 2: ')
+b='hello python'
+print(b[5:])
+print('tamanho:')
+print(len(b))
