de803a5f9f5dbe27b6e438251d82b7d0aa0b15c3
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-03-31 22:14:09 -0300

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,30 @@
+if True:
+    print('It was true!')
+    x = False
+    if x:
+        print('x was True!')
+    else:
+        print('i will be printed in any case where x is not true')
+        #O PYTHON TRABALHA POR IDENTAÇÃO!!!!
+loc='bank'
+if loc == 'auto shop':
+    print('welcome to the autoo shop')
+elif loc == 'bank':
+    print('welcome to the bank!')
+else:
+    print('where are you?')
+
+person = 'sammy'
+if person == 'sammy':
+    print('welcome sammy')
+else:
+    print('welcome, whats your name?')
+
+person = 'george'
+if person =='sammy':
+    print('welcome sammy')
+elif person == 'george':
+    print('welcome george')
+else:
+    print('welcome, whats your name? ')
+
