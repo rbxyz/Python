74656525441147ab8aa8b3af3efdc3a179ae486c
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-03-31 22:17:41 -0300

    Create create_list.py

---
+++ create_list.py
@@ -0,0 +1,7 @@
+#introdução à sequencias
+my_list = [1,2,3]
+#criamos uma lista de numeros inteiro
+#listas podem armazenar variaveis diferentes
+were_list = [1,'string', 20, 23, 'list']
+len(were_list)
+#len() irá dizer quantos intesn tem na sequencia
