d1033d11e65d788f9800a1effb734ac90c51c94d
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-03-31 22:22:18 -0300

    Create tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,20 @@
+#elas são semelhantes às listas, no entanto tuplas não podem ser alteradas
+#usar para criar variaveis para não alterar
+#CONSTRUINDO TUPLAS
+t=(1,2,3)
+print(len(t))
+print(t=("one",2))
+print(t)
+print(t[0])
+#indexação funciona bem
+print(t[-1])
+#corte de dados também...
+#MÉTODOSS BÁSICOS DA TUPLA
+#tuplas tem métodos internos, mas n~~ao tanto como as listas
+print(t.index('one'))
+print(t.count('one'))
+#IMUTABILIDADE
+#É UTIIZADAD MAIS EM CASOS QUE SÃO NECESSÁRIAS AS IMUTAÇÕESS EM PROGRAMAS
+#NÃO SÃO MUITO USADAS,, SÓ NESSES CASOS
+
+
