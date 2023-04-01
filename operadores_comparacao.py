6762943f3d400c59fdd69f91b09c684f9e9ca53d
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-03-31 22:18:53 -0300

    Create operadores_comparacao.py

---
+++ operadores_comparacao.py
@@ -0,0 +1,25 @@
+#agora, nesta secção aprederemos a reproduzir valores booleanos
+#     ==      se os dois forem iguais, retorna verdadeiro                               (a==b)    
+#     !=      se não forem iguais, retorna verdadeiro                                   (a!=b)
+#     >       se valor esquerdo maior que o direito, retorna verdadeiro                 (a>b)
+#     <       se valor direito maior que o esquerdo, retorna verdadeiro                 (a<b)
+#     >=      se valor esquerdo maior ou igual ao direito, retorna verdadeiro           (a>=b)
+#     <=      se valor direito maior ou igual ao esquerdo, retorna verdadeiro           (a<=b)
+#------------------------------------------------------------------------------------------------------------------
+#operadores em comparação em cadeia 
+#uma carac. interessante do py é  a capacidade de encadear comparações multiplas para realizar um teste mais complexo
+#exemplo de uso de cadeias
+print(1<2<3)
+print(1<2 and 2<3)
+print('------------------------------')
+print(1<3>2)
+print(1<3 and 3>2)
+print('------------------------------')
+print("aqui ele compara, sendo que precisa de somente de um para fungar, para dar valor true")
+print(1==2 or 2<3)
+print(1==1 or 100==1)
+print('------------------------------')
+
+
+
+
