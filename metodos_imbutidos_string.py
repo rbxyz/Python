6399d6f3cb13fd5663b6cc322427cddd15090f5b
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-03-31 22:20:26 -0300

    Create metodos_imbutidos_string.py

---
+++ metodos_imbutidos_string.py
@@ -0,0 +1,10 @@
+#as strings possuem alguns métodos internos que podem se assemelhar em operações de expressão regular
+#pode usar split() para dividir a string em um determinado elemento e retornar uma lista do resultado
+#pode-se usar partition() para retornar um tuplla que inclui o separador (a primeira ocorrencia) e a primeira metade 
+#final
+s='hello'
+split=s.split('e')
+print(split)
+part=s.partition('e')
+print(part)
+#fazer os exeercicios da lista avançada de strings
